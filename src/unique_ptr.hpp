907c8747e6c7cb7e6bd3855777323ec735ecd3b2
johnfifty <85110707+johnfifty@users.noreply.github.com>
2021-06-22 11:53:21 +0300

    fixes to unique_ptr

--- src/unique_ptr.hpp
+++ src/unique_ptr.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <iostream>
+#include <utility>
 
 namespace imp {
 
@@ -10,42 +10,41 @@ class unique_ptr
   T* ptr = nullptr;
 
 public:
-  unique_ptr() = default;
+  unique_ptr() noexcept = default;
+  explicit unique_ptr(T* arg) noexcept
+    : ptr{ arg }
+  {}
+
+  unique_ptr(unique_ptr const&) = delete;
+  unique_ptr& operator=(unique_ptr const&) = delete;
+
+  unique_ptr(unique_ptr&& arg) noexcept
+    : ptr{ std::move(arg.ptr) }
+  {
+    arg.ptr = nullptr;
+  }
+  unique_ptr& operator=(unique_ptr&& arg) noexcept
+  {
+    delete ptr;
+    ptr = std::move(arg.ptr);
+    arg.ptr = nullptr;
+    return *this;
+  }
+
   unique_ptr(std::nullptr_t) noexcept {}
   unique_ptr& operator=(std::nullptr_t) noexcept
   {
+    delete ptr;
     ptr = nullptr;
     return *this;
   }
 
-  explicit unique_ptr(T* ptr_) noexcept
-    : ptr{ ptr_ }
-  {
-    std::cout << "copy\n";
-  }
+  ~unique_ptr() { delete ptr; }
 
-  unique_ptr(unique_ptr const& a) = delete;
-  unique_ptr& operator=(unique_ptr const& a) = delete;
-
-  unique_ptr(unique_ptr&& second) noexcept
-  {
-    if (ptr != nullptr) {
-      delete ptr;
-    }
-    ptr = second.ptr;
-    second = nullptr;
-    std::cout << "move constructor\n";
-  }
-
-  ~unique_ptr()
-  {
-    std::cout << "destruct\n";
-    delete ptr;
-  }
-
-  T* get() const noexcept { return ptr; }
-
+  [[nodiscard]] T* get() const noexcept { return ptr; }
   T& operator*() const { return *ptr; }
+
+  explicit operator bool() const noexcept { return ptr != nullptr; }
 };
 
 template <typename T>
